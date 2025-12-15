796ba8e59a3e6c96ed501c237d4465be3a03905a
car8092361628 <antonioacr5665>
2025-12-15 19:31:35 -0400

    Ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+class Animal:
+    def hablar(self):
+        print("Este animal hace un sonido.")
+
+class Perro(Animal):
+    def hablar(self):
+        print("El perro dice: ¡Guau guau!")
+
+class Gato(Animal):
+    def hablar(self):
+        print("El gato dice: ¡Miau miau!")
